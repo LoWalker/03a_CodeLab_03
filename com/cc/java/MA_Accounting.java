6356527b36a29efe89047da28620fe5901b9085a
LoWalker <lawrence.langen@gmail.com>
2023-02-02 21:52:11 +0100

    2nd Commit

---
+++ com/cc/java/MA_Accounting.java
@@ -0,0 +1,24 @@
+package com.cc.java;
+
+public class MA_Accounting extends Mitarbeiter {
+    
+
+public MA_Accounting(String lastName, String firstName, int dateOfBirth ) {
+    super(lastName, firstName, dateOfBirth);
+}
+
+public String doYourWork(String flag) {
+   switch (key) {
+        case value:
+          return this.getFirstName()
+        case value:
+        
+        break;
+        case value:
+        
+        break;
+    default:
+        break;"ERROR";
+   } 
+}
+}
